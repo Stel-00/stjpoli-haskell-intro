a7366b0554c968f61ec3e1d92653c2e8478a5ce3
Stel-00 <stel@poli.cc>
2025-09-12 01:49:53 +0200

    Final commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ghci to start haskelling + file to load file
 :l :load to load a file
 :q :quit to quit
+Difficulties:
+I was unsure how to go about creating the second phase of input where the user give his list of numbers
+Can prbably be fixed later with some help
--- intro.hs
+++ intro.hs
@@ -1,8 +1,19 @@
-tal :: Int -> Int
-tal x = x
+main :: IO () -- Lines 1-4 inspiration taken from the solution to kattis problem
+main = do
+    inpt <- getLine
+    let n = read inpt
+    let lst = [4, 5, 7, 4, 2, 7, 9, 2] -- Improvised list instead of user input
+        m = if even n
+            then div (n+1) 2
+            else  div n 2
+        sortedlist = recsort lst
+        top = take m (reverse sortedlist)
+        res = sum top
+    print res
 
-summera n a b c d e =
-    return [a, b, c, d, e]
-
-
-
+recsort :: [Int] -> [Int] -- Recursive sorting function (acending order) aka quicksort
+recsort [] = [] -- Base case
+recsort (x:xs) = recsort [y | y <- xs, y < x] ++ [x] ++ recsort [y | y <- xs, y >= x] 
+-- To the left all numbers lower than x
+-- To the right all numbers greater or eaual to x
+-- In the middle x
